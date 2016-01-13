0b009ada24be6cd09e417f21abf4ec5c3b1919b1
Xander May <liesonrs@gmail.com>
2016-01-13 13:44:17 -0500

    Start of the inv header

---
+++ WebSlash/src/inv.cpp
@@ -0,0 +1,17 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <time.h>
+#include <Windows.h>
+#include <iostream>
+#include "inv.h"
+
+
+
+void dispInv (char*){
+
+
+}
+
+
---
+++ WebSlash/src/inv.h
@@ -0,0 +1,13 @@
+#ifndef INV_H
+#define INV_H
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <time.h>
+#include <windows.h>
+
+void dispInv();
+
+#endif
