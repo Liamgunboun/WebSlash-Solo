e970abf9180be2835569db518155b5509566cf2f
Xander May <liesonrs@gmail.com>
2016-01-14 15:12:24 -0500

    Chanegd to inv

--- WebSlash/src/inv.cpp
+++ WebSlash/src/inv.cpp
@@ -7,10 +7,27 @@
 #include <iostream>
 #include "inv.h"
 
+#define MAX_HOARD 50
+#define MAX_NAME_LEN 60
 
 
-void dispInv (char*){
 
+void dispInv (){
+    FILE* invFile;
+    invFile = fopen("../playerFiles/playerInv");
+    char invStore [MAX_HOARD][MAX_NAME_LEN];
+
+    if (lvlFile==NULL){
+        printf("{Critical error 50}");
+        return 0;
+    }
+
+    for(int i = 0; i < max)
+
+
+}
+
+void addToInv (char*, int itemClass){
 
 }
 
